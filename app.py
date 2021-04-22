db01f6590e6881c1d67e37a5da4c032212ab29f4
Ben chelbi Wissem <Ben_chelbiw@yahoo.com>
2021-04-22 10:01:24 +0200

    we

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def index():
     return flask.render_template('index.html')
 
 def ValuePredictor(to_predict_list):
-    to_predict = np.array(to_predict_list).reshape(1,4)
+    to_predict = np.array(to_predict_list).reshape(1,17)
     loaded_model = pickle.load(open("model.pkl","rb"))
     result = loaded_model.predict(to_predict)
     return result[0]
